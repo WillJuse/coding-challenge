082805ae422f39b11790223f30ad050c1c71c79a
WillJuse <willjuszczyk@gmail.com>
2016-12-28 15:36:51 -0500

    adding a model to the back end

---
+++ ambassador/referral/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+# Create your models here.
+@python_2_unicode_compatible
+class Referrals(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    clicks = models.IntegerField(default=1)
+
+    def __str__(self):
+        return "Name: {}".format(self.name);
